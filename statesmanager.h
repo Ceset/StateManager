a49e458cc785db7f58d420c55c9d42c9cfada842
Hakki Ozturk <h_ozturk93@windowslive.com>
2014-05-11 19:44:33 +0000

    Fixed removeAllStatesExcept and added comments.

--- statesmanager.h
+++ statesmanager.h
@@ -6,23 +6,44 @@
 #include <algorithm>
 #include "state.h"
 
+// This class is a manager to contain and manage
+// all state classes. If you ever desires to add,
+// remove or anything that is wanted this class
+// just use functions that are provided by the
+// manager class.
+//
+// In most cases the last added states will be 
+// rendered to the top. So it will just update, 
+// render according to adding order.
+//
+// If you need furture features you should make 
+// changes at the class.
+
 class StatesManager
 {
 public:
     StatesManager();
 
+    // A function to add a new state. It will require a unique
+    // state identifier.
     void addState(std::unique_ptr<State> state, StateID identifier);
+    // Get the state as a pointer. If needs be.
     State* getState(StateID identifier);
 
+    // Just remove one specific state.
     void removeState(StateID identifier);
+    // Remove all added state except one specific state.
     void removeAllStatesExcept(StateID identifier);
+    // Remove all added states.
     void removeAllStates();
 
-    void checkStates();
+    // Updates all added states.
     void updateStates();
+    // Renders all added states.
     void renderStates();
 
 private:
+    // The container of our states.
     std::map<StateID, std::unique_ptr<State>> mStates;
 };
 
